44ea38694923a05b9c66df0f2f8a8e33302de2d6
AnanyashreeGarg <ananyashree.garg@gmail.com>
2017-06-28 22:03:09 +0530

    linear linked list operations

--- linkedlist.c
+++ linkedlist.c
@@ -2,69 +2,82 @@
 #include<stdio.h>
 #include<stdlib.h>
 void insertFirst(struct linkedList * head, int ele){
-struct node *link = (struct node*) malloc (sizeof(struct node));
-link->element = ele;
-link->next = head->first;
-head -> first = link;
-head -> count ++;
+	struct node *link = (struct node*) malloc (sizeof(struct node));
+	link->element = ele;
+	link->next = head->first;
+	head -> first = link;
+	head -> count ++;
+	printList(head);
 }
 
-struct node* deleteFirst(struct linkedList * head){
-struct node *ptr=head->first;
-head->first=ptr->next;
-head->count--;
-return head->first;
+void insertEnd (struct linkedList * head, int ele){
+	struct node* temp=(struct node*) malloc (sizeof(struct node));
+	temp->element=ele;
+	temp->next=NULL;
+	struct node* curr=head->first;
+	if(curr==NULL)
+	{insertFirst(head,ele);
+	//return;
+	}
+	else{
+	while(curr->next!=NULL)
+		curr=curr->next;
+	curr->next=temp;
+	head->count++;
+	printList(head);
+	}
+}
+
+
+
+void deleteFirst(struct linkedList * head){
+	struct node *ptr=head->first;
+	if(ptr==NULL)
+	{	
+		printf("\n-2");
+		return;
+	}
+	printf("\n%d",ptr->element);
+	head->first=ptr->next;
+	head->count--;
+	
+}
+
+void deleteEnd(struct linkedList* head)
+{
+	struct node* curr=head->first;
+	struct node* prev=head->first;
+	if(curr==NULL)
+	{	
+		printf("\n-2");
+		return;
+	}
+	curr=curr->next;
+	if(curr==NULL)
+	deleteFirst(head);
+	else
+	{
+	while(curr->next!=NULL)
+	{
+		prev=prev->next;
+		curr=curr->next;
+	}
+	prev->next=NULL;
+	head->count--;
+	printf("\n%d",curr->element);
+	}
 }
 
 void printList(struct linkedList * head){
-struct node *ptr = head->first; 
-printf("\n[ ");
-while(ptr != NULL){
-      printf("%d, ", ptr->element);
-      ptr = ptr->next;
+	struct node *ptr = head->first; 
+	printf("\n");
+	while(ptr != NULL){
+     		printf("%d\t", ptr->element);
+      		ptr = ptr->next;
    }
-	printf(" ]\n");
 }
  
-int search(struct linkedList *head,int ele)
-{
-struct node* temp=head->first;
-int found=0;
-while(temp!=NULL)
-{
-	if(temp->element==ele)
-	{
-		found=1;
-		return found;
-	}
-	temp=temp->next;
-}
-if(!found)
-return found;
-else 
-return 1;
-}
 
-struct node* delete(struct linkedList* head,int ele)
-{
-struct node* curr=head->first;
-struct node* prev=head->first;
-if(curr->element==ele)
-{
-	head->first=deleteFirst(head);
-	head->count--;
-	return head->first;
-}
-while(curr->next!=NULL)
-{
-	curr=curr->next;
-	if(curr->element==ele)
-	{
-		prev->next=curr->next;
-		head->count--;
-		return head->first;
-	}
-	prev=prev->next;
-}
-return head->first;
-}
+
+
+
